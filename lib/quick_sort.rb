f98f0675c94a83f416405ee291dcbe09a6a8710a
Philip Nguyen <supertaru@gmail.com>
2014-10-19 19:15:36 -0700

    rubocop and refactor for readability

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -1,15 +1,25 @@
 # Extending existing array with quick_sort
 class Array
   def quick_sort
-    return self if self.size <= 1
-    arr = self.dup
+    return self if size <= 1
+    arr = dup
     pivot_i = Random.new.rand(arr.size - 1)
     pivot = arr[pivot_i]
 
-    i = -1
-    smaller = arr.select { |num| num if num < pivot }
-    larger = arr.select { |num| i += 1; num if num >= pivot && i != pivot_i }
+    smaller(arr, pivot).quick_sort +
+    [pivot] +
+    larger(arr, pivot, pivot_i).quick_sort
+  end
 
-    smaller.quick_sort + [pivot] + larger.quick_sort
+  def smaller(arr, pivot)
+    arr.select { |num| num if num < pivot }
+  end
+
+  def larger(arr, pivot, pivot_i)
+    i = -1
+    arr.select do |num|
+      i += 1
+      num if num >= pivot && i != pivot_i
+    end
   end
 end
